3c0c1f1c7ca3c0dd236ed4fbf19370b4c5c93963
Navaneeth <153580901+Navaneeth1818@users.noreply.github.com>
2026-04-11 23:32:07 +0530

    Add pathRewrite option for proxy middleware

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -4,28 +4,35 @@ const cors = require("cors");
 
 const app = express();
 
-// Allow frontend to connect
 app.use(cors());
 
 // USER SERVICE
 app.use("/users", createProxyMiddleware({
     target: "https://user-services-u4rn.onrender.com",
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        "^/users": "/users"
+    }
 }));
 
 // PRODUCT SERVICE
 app.use("/products", createProxyMiddleware({
     target: "https://product-service-mw02.onrender.com",
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        "^/products": "/products"
+    }
 }));
 
 // ORDER SERVICE
 app.use("/orders", createProxyMiddleware({
     target: "https://order-service-119e.onrender.com",
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        "^/orders": "/orders"
+    }
 }));
 
-// Start Gateway
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`API Gateway running on port ${PORT}`);
